c1538ad596efcb2379d44d0611209ca0118de401
Harriebo <harrie.hoogeveen@gmail.com>
2026-02-04 22:59:31 +0100

    Fix #112: Add TrueTypeFont classes for reflection (#115)
    * Fix #112: Add TrueTypeFont classes for reflection
    
    * Add reflection classnames as string sins they are not public

--- quarkus-openpdf/deployment/src/main/java/io/quarkiverse/itext/openpdf/deployment/OpenPDFProcessor.java
+++ quarkus-openpdf/deployment/src/main/java/io/quarkiverse/itext/openpdf/deployment/OpenPDFProcessor.java
@@ -93,6 +93,8 @@ class OpenPDFProcessor {
         classNames.add(com.lowagie.text.Utilities.class.getName());
         classNames.add(com.lowagie.text.pdf.PdfName.class.getName());
         classNames.add(com.lowagie.text.pdf.internal.PdfVersionImp.class.getName());
+        classNames.add("com.lowagie.text.pdf.TrueTypeFont");
+        classNames.add("com.lowagie.text.pdf.TrueTypeFontUnicode");
 
         Log.debugf("OpenPDF Reflection: %s", classNames);
         // methods and fields
